b6132d58a2a2a384654fc0507146254237363477
Brad Robinson <brobinson@toptensoftware.com>
2011-04-29 13:21:00 +1000

    Added KeepConnectionAlive

--- PetaPoco/PetaPoco.cs
+++ PetaPoco/PetaPoco.cs
@@ -200,9 +200,13 @@ namespace PetaPoco
 		// Automatically close one open shared connection
 		public void Dispose()
 		{
-			if (_sharedConnectionDepth > 0)
-				CloseSharedConnection();
+			// Automatically close one open connection reference
+			//  (Works with KeepConnectionAlive and manually opening a shared connection)
+			CloseSharedConnection();
 		}
+
+		// Set to true to keep the first opened connection alive until this object is disposed
+		public bool KeepConnectionAlive { get; set; }
 
 		// Open a connection (can be nested)
 		public void OpenSharedConnection()
@@ -212,6 +216,9 @@ namespace PetaPoco
 				_sharedConnection = _factory.CreateConnection();
 				_sharedConnection.ConnectionString = _connectionString;
 				_sharedConnection.Open();
+
+				if (KeepConnectionAlive)
+					_sharedConnectionDepth++;		// Make sure you call Dispose
 			}
 			_sharedConnectionDepth++;
 		}
@@ -219,11 +226,14 @@ namespace PetaPoco
 		// Close a previously opened connection
 		public void CloseSharedConnection()
 		{
-			_sharedConnectionDepth--;
-			if (_sharedConnectionDepth == 0)
+			if (_sharedConnectionDepth > 0)
 			{
-				_sharedConnection.Dispose();
-				_sharedConnection = null;
+				_sharedConnectionDepth--;
+				if (_sharedConnectionDepth == 0)
+				{
+					_sharedConnection.Dispose();
+					_sharedConnection = null;
+				}
 			}
 		}
 
