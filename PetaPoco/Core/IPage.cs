4f5a525f034d7e62a58896bf0d35c0775318dd91
Aaron Throckmorton <aaron@drsus.com>
2013-05-23 17:47:14 -0600

    Added interface IPage, and Page now inherits from it.  This allows for a partial view in MVC to receive an IPage as it's model and be used to generate paging buttons for any type of Page<> result.

---
+++ PetaPoco/Core/IPage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PetaPoco
+{
+    public interface IPage
+    {
+        /// <summary>
+        /// The current page number contained in this page of result set 
+        /// </summary>
+        public long CurrentPage { get; set; }
+
+        /// <summary>
+        /// The total number of pages in the full result set
+        /// </summary>
+        public long TotalPages { get; set; }
+
+        /// <summary>
+        /// The total number of records in the full result set
+        /// </summary>
+        public long TotalItems { get; set; }
+
+        /// <summary>
+        /// The number of items per page
+        /// </summary>
+        public long ItemsPerPage { get; set; }
+
+        /// <summary>
+        /// User property to hold anything.
+        /// </summary>
+        public object Context { get; set; }
+    }
+}
