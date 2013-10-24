0644f024a9ae57c55c8ac743bb4ad8559f34b9bc
LankyPaddy <paddy-git@oollyfoot.org>
2013-10-24 20:33:31 +0100

    Multi-result set support
    Added support for multi-result set database queries

--- PetaPoco/Database.cs
+++ PetaPoco/Database.cs
@@ -1969,6 +1969,45 @@ namespace PetaPoco
 
 		#endregion
 
+        #region operation: Multi-Result Set
+        /// <summary>
+        /// Perform a multi-results set query
+        /// </summary>
+        /// <param name="sql">An SQL builder object representing the query and it's arguments</param>
+        /// <returns>A GridReader to be queried</returns>
+        public GridReader QueryMultiple(Sql sql)
+        {
+            return QueryMultiple(sql.SQL, sql.Arguments);
+        }
+
+        /// <summary>
+        /// Perform a multi-results set query
+        /// </summary>
+        /// <param name="sql">The SQL query to be executed</param>
+        /// <param name="args">Arguments to any embedded parameters in the SQL</param>
+        /// <returns>A GridReader to be queried</returns>
+        public GridReader QueryMultiple(string sql, params object[] args)
+        {
+            OpenSharedConnection();
+
+            GridReader result = null;
+
+            var cmd = CreateCommand(_sharedConnection, sql, args);
+
+            try
+            {
+                var reader = cmd.ExecuteReader();
+                result = new GridReader(this, cmd, reader);
+            }
+            catch (Exception x)
+            {
+                if (OnException(x))
+                    throw;
+            }
+            return result;
+        }
+        #endregion
+
 		#region Last Command
 
 		/// <summary>
