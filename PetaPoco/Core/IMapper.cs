a7a936d9b26866d1419ae606c8a595889c4ff41a
Wade <wade@boffindevelopments.com>
2015-12-06 21:31:25 +1000

    Fixes parameter casing

--- PetaPoco/Core/IMapper.cs
+++ PetaPoco/Core/IMapper.cs
@@ -43,21 +43,21 @@ namespace PetaPoco
         /// <summary>
         ///     Supply a function to convert a database value to the correct property value
         /// </summary>
-        /// <param name="TargetProperty">The target property</param>
-        /// <param name="SourceType">The type of data returned by the DB</param>
+        /// <param name="targetProperty">The target property</param>
+        /// <param name="sourceType">The type of data returned by the DB</param>
         /// <returns>A Func that can do the conversion, or null for no conversion</returns>
-        Func<object, object> GetFromDbConverter(PropertyInfo TargetProperty, Type SourceType);
+        Func<object, object> GetFromDbConverter(PropertyInfo targetProperty, Type sourceType);
 
         /// <summary>
         ///     Supply a function to convert a property value into a database value
         /// </summary>
-        /// <param name="SourceProperty">The property to be converted</param>
+        /// <param name="sourceProperty">The property to be converted</param>
         /// <returns>A Func that can do the conversion</returns>
         /// <remarks>
         ///     This conversion is only used for converting values from POCO's that are
         ///     being Inserted or Updated.
         ///     Conversion is not available for parameter values passed directly to queries.
         /// </remarks>
-        Func<object, object> GetToDbConverter(PropertyInfo SourceProperty);
+        Func<object, object> GetToDbConverter(PropertyInfo sourceProperty);
     }
 }
