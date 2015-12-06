f32262d11466212141e05a8411ac5945387625a6
Wade <wade@boffindevelopments.com>
2015-12-06 21:30:43 +1000

    Adds parameter null check

--- PetaPoco/Database.cs
+++ PetaPoco/Database.cs
@@ -1,8 +1,8 @@
-﻿// <copyright file="Database.cs" company="PetaPoco - CollaboratingPlatypus">
+﻿// <copyright company="PetaPoco - CollaboratingPlatypus">
 //      Apache License, Version 2.0 https://github.com/CollaboratingPlatypus/PetaPoco/blob/master/LICENSE.txt
 // </copyright>
 // <author>PetaPoco - CollaboratingPlatypus</author>
-// <date>2015/12/05</date>
+// <date>2015/12/06</date>
 
 using System;
 using System.Collections.Generic;
@@ -1195,6 +1195,8 @@ namespace PetaPoco
         /// </remarks>
         public object Insert(object poco)
         {
+            if (poco == null)
+                throw new ArgumentNullException("poco");
             var pd = PocoData.ForType(poco.GetType());
             return Insert(pd.TableInfo.TableName, pd.TableInfo.PrimaryKey, pd.TableInfo.AutoIncrement, poco);
         }
