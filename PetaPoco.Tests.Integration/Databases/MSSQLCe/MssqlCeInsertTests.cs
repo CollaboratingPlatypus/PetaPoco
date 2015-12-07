f503d45b38751ee3eb75ead974ffdb98d20ff13b
Wade <wade@boffindevelopments.com>
2015-12-07 13:33:04 +1000

    Includes initial support for all db types and integration testing

---
+++ PetaPoco.Tests.Integration/Databases/MSSQLCe/MssqlCeInsertTests.cs
@@ -0,0 +1,20 @@
+﻿// <copyright company="PetaPoco - CollaboratingPlatypus">
+//      Apache License, Version 2.0 https://github.com/CollaboratingPlatypus/PetaPoco/blob/master/LICENSE.txt
+// </copyright>
+// <author>PetaPoco - CollaboratingPlatypus</author>
+// <date>2015/12/06</date>
+
+using PetaPoco.Tests.Integration.Databases.MSSQLCe;
+using Xunit;
+
+namespace PetaPoco.Tests.Integration.Databases.MSSQLCe
+{
+    [Collection("MssqlCeTests")]
+    public class MssqlCeInsertTests : BaseInsertTests
+    {
+        public MssqlCeInsertTests()
+            : base(new MssqlCeDBTestProvider())
+        {
+        }
+    }
+}
