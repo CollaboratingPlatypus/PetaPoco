e5199798dc0d3e67166809f5ebff30bf1bd96d5f
Wade <wade@boffindevelopments.com>
2016-01-24 15:39:39 +1000

    Cleans up transaction code and add doc comments

--- PetaPoco/Core/Transaction.cs
+++ PetaPoco/Core/Transaction.cs
@@ -1,18 +1,11 @@
-﻿// <copyright file="Transaction.cs" company="PetaPoco - CollaboratingPlatypus">
+﻿// <copyright company="PetaPoco - CollaboratingPlatypus">
 //      Apache License, Version 2.0 https://github.com/CollaboratingPlatypus/PetaPoco/blob/master/LICENSE.txt
 // </copyright>
 // <author>PetaPoco - CollaboratingPlatypus</author>
-// <date>2015/12/05</date>
-
-using System;
+// <date>2016/01/24</date>
 
 namespace PetaPoco
 {
-    public interface ITransaction : IDisposable
-    {
-        void Complete();
-    }
-
     /// <summary>
     ///     Transaction object helps maintain transaction depth counts
     /// </summary>
