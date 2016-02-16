eb75de7c87e35bcd52dfc49d348f965575a2319b
Wade <wade@boffindevelopments.com>
2016-02-16 22:50:21 +1000

    Adds integration testing for nullable primary keys and better inserting of anonymous types

--- PetaPoco.Tests.Integration/Databases/BaseInsertTests.cs
+++ PetaPoco.Tests.Integration/Databases/BaseInsertTests.cs
@@ -2,7 +2,7 @@
 //      Apache License, Version 2.0 https://github.com/CollaboratingPlatypus/PetaPoco/blob/master/LICENSE.txt
 // </copyright>
 // <author>PetaPoco - CollaboratingPlatypus</author>
-// <date>2016/01/06</date>
+// <date>2016/02/16</date>
 
 using System;
 using PetaPoco.Tests.Integration.Models;
@@ -13,6 +13,12 @@ namespace PetaPoco.Tests.Integration.Databases
 {
     public abstract class BaseInsertTests : BaseDatabase
     {
+        private readonly Note _note = new Note
+        {
+            Text = "PetaPoco's note",
+            CreatedOn = new DateTime(1948, 1, 11, 4, 2, 4, DateTimeKind.Utc)
+        };
+
         private readonly Order _order = new Order
         {
             PoNumber = "Peta's Order",
@@ -44,10 +50,11 @@ namespace PetaPoco.Tests.Integration.Databases
         [Fact]
         public void Insert_GivenPoco_ShouldBeValid()
         {
-            DB.Insert(_person);
+            var id = DB.Insert(_person);
 
             var personOther = DB.Single<Person>(_person.Id);
 
+            _person.Id.ShouldBe(id);
             personOther.ShouldNotBeNull();
             personOther.ShouldBe(_person);
         }
@@ -113,27 +120,14 @@ namespace PetaPoco.Tests.Integration.Databases
         }
 
         [Fact]
-        public void Insert_GivenTableNamePrimaryKeyNameAndAnonymousType_ShouldInsertPoco()
-        {
-            var note = new { Text = "Test note", CreatedOn = new DateTime(1945, 1, 12, 5, 9, 4, DateTimeKind.Utc) };
-
-            var key = DB.Insert("Note", "Id", note);
-            var otherNote = DB.Single<Note>(key);
-
-            key.ShouldNotBeNull();
-            otherNote.Text.ShouldBe(note.Text);
-            otherNote.CreatedOn.ShouldBe(note.CreatedOn);
-        }
-
-        [Fact]
         public void Insert_GivenTableNameAndAnonymousType_ShouldInsertPoco()
         {
             var log = new { Description = "Test log", CreatedOn = new DateTime(1945, 1, 12, 5, 9, 4, DateTimeKind.Utc) };
 
-            var logKey = DB.Insert("TransactionLogs", log);
+            var logId = DB.Insert("TransactionLogs", log);
             var otherLog = DB.Single<TransactionLog>($"SELECT * FROM {DB.Provider.EscapeTableName("TransactionLogs")}");
 
-            logKey.ShouldBeNull();
+            logId.ShouldBeNull();
             otherLog.Description.ShouldBe(log.Description);
             otherLog.CreatedOn.ShouldBe(log.CreatedOn);
         }
@@ -141,7 +135,7 @@ namespace PetaPoco.Tests.Integration.Databases
         [Fact]
         public void Insert_GivenNullByteArray_ShouldNotThrow()
         {
-            DB.Insert("BugInvestigation_10R9LZYK", "Id", true, new { TestColumn1 = (byte[])null });
+            DB.Insert("BugInvestigation_10R9LZYK", "Id", true, new { TestColumn1 = (byte[]) null });
             DB.ExecuteScalar<int>($"SELECT * FROM {DB.Provider.EscapeTableName("BugInvestigation_10R9LZYK")}").ShouldBe(1);
         }
 
@@ -156,12 +150,94 @@ namespace PetaPoco.Tests.Integration.Databases
         public void Insert_GivenPocoWithNullDateTime_ShouldNotThrow()
         {
             _person.Dob = null;
-            DB.Insert(_person);
+            var id = DB.Insert(_person);
 
             var personOther = DB.Single<Person>(_person.Id);
 
+            _person.Id.ShouldBe(id);
             personOther.ShouldNotBeNull();
             personOther.ShouldBe(_person);
+        }
+
+        [Fact]
+        public void Insert_GivenConventionalPocoWithTable_ShouldInsertPoco()
+        {
+            var id = DB.Insert("People", _person);
+
+            var otherPerson = DB.Single<Person>(id);
+
+            _person.Id.ShouldBe(id);
+            otherPerson.ShouldNotBeNull();
+            otherPerson.ShouldBe(_person);
+        }
+
+        [Fact]
+        public void Insert_GivenConventionalPocoWithTableAndPrimaryKey_ShouldInsertPoco()
+        {
+            var id = DB.Insert("People", "Id", _person);
+
+            var otherPerson = DB.Single<Person>(id);
+
+            _person.Id.ShouldBe(id);
+            otherPerson.ShouldNotBeNull();
+            otherPerson.ShouldBe(_person);
+        }
+
+        [Fact]
+        public void Insert_GivenPocoWithPrimaryNullableValueType_ShouldBeValid()
+        {
+            var note = new NoteNullablePrimary() { Text = _note.Text, CreatedOn = _note.CreatedOn };
+
+            var id = DB.Insert(note);
+
+            var noteOther = DB.Single<NoteNullablePrimary>(note.Id);
+
+            note.Id.ShouldBe(id);
+            noteOther.ShouldNotBeNull();
+            noteOther.ShouldBe(note);
+        }
+
+        [Fact]
+        public void Insert_GivenTableNamePrimaryKeyNameAndAnonymousType_ShouldInsertPoco()
+        {
+            var note = new { Text = "Test note", CreatedOn = new DateTime(1945, 1, 12, 5, 9, 4, DateTimeKind.Utc) };
+
+            var id = DB.Insert("Note", "Id", note);
+
+            var otherNote = DB.Single<Note>(id);
+            
+            otherNote.Text.ShouldBe(note.Text);
+            otherNote.CreatedOn.ShouldBe(note.CreatedOn);
+        }
+
+        [Fact]
+        public void Insert_GivenTableNamePrimaryKeyNameAndAnonymousTypeWithNullablePrimaryKey_ShouldInsertPoco()
+        {
+            var note = new { Id = (int?) null, Text = "Test note", CreatedOn = new DateTime(1945, 1, 12, 5, 9, 4, DateTimeKind.Utc) };
+
+            var id = DB.Insert("Note", "Id", note);
+            var otherNote = DB.Single<Note>(id);
+
+            id.ShouldNotBeNull();
+            otherNote.Text.ShouldBe(note.Text);
+            otherNote.CreatedOn.ShouldBe(note.CreatedOn);
+        }
+
+        [Fact]
+        public void Insert_GivenTableNamePrimaryKeyNameAndAnonymousTypeWithStaticPrimaryKey_ShouldInsertPoco()
+        {
+            var person = new { Id = Guid.NewGuid(), Age = 18, Dob = new DateTime(1945, 1, 12, 5, 9, 4, DateTimeKind.Utc), Height = 180, FullName = "Peta" };
+
+            var id = DB.Insert("People", "Id", person);
+
+            var otherPerson = DB.Single<Person>(id);
+
+            id.ShouldNotBeNull();
+            otherPerson.Id.ShouldBe(person.Id);
+            otherPerson.Age.ShouldBe(person.Age);
+            otherPerson.Dob.ShouldBe(person.Dob);
+            otherPerson.Height.ShouldBe(person.Height);
+            otherPerson.Name.ShouldBe(person.FullName);
         }
     }
 }
