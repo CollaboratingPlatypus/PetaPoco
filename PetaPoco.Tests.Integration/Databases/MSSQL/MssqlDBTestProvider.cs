315b4e5e544e4a452d965b561a940256a8a1df83
Wade Baglin <plebmail@gmail.com>
2018-06-06 15:04:06 +1000

    Update SQL Server integration tests to use docker-compose
      - Removes need to have SQL Server installed locally

--- PetaPoco.Tests.Integration/Databases/MSSQL/MssqlDBTestProvider.cs
+++ PetaPoco.Tests.Integration/Databases/MSSQL/MssqlDBTestProvider.cs
@@ -4,12 +4,33 @@
 // <author>PetaPoco - CollaboratingPlatypus</author>
 // <date>2015/12/13</date>
 
+using System;
+using System.Linq;
+
 namespace PetaPoco.Tests.Integration.Databases.MSSQL
 {
     public class MssqlDBTestProvider : DBTestProvider
     {
-        protected override IDatabase Database => DatabaseConfiguration.Build().UsingConnectionStringName("mssql").Create();
+        private string _connectionStringKey = "mssql";
+
+        protected override IDatabase Database => DatabaseConfiguration.Build().UsingConnectionStringName(_connectionStringKey).Create();
 
         protected override string ScriptResourceName => "PetaPoco.Tests.Integration.Scripts.MSSQLBuildDatabase.sql";
+
+        public override IDatabase Execute()
+        {
+            _connectionStringKey = "mssql_builder";
+            base.Execute();
+            _connectionStringKey = "mssql";
+            return Database;
+        }
+
+        public override void ExecuteBuildScript(IDatabase database, string script)
+        {
+            script.Split(new [] { "GO"}, StringSplitOptions.RemoveEmptyEntries).ToList().ForEach(s =>
+            {
+                base.ExecuteBuildScript(database, s);
+            });
+        }
     }
 }
