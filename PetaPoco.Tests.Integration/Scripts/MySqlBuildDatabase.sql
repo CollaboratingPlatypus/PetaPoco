0c2f507cd1e5b99b19a506884a7efa4eebf77060
Aaron Sherber <aaron@sherber.com>
2018-11-05 11:20:34 -0500

    Add MySql and MariaDb integration tests

--- PetaPoco.Tests.Integration/Scripts/MySqlBuildDatabase.sql
+++ PetaPoco.Tests.Integration/Scripts/MySqlBuildDatabase.sql
@@ -90,3 +90,30 @@ CREATE TABLE `BugInvestigation_3F489XV0` (
 	`TC3` INT NOT NULL,
 	`TC4` INT NOT NULL
 ) ENGINE=INNODB;
+
+-- Stored Procedures
+DROP PROCEDURE IF EXISTS `SelectPeople`;
+DROP PROCEDURE IF EXISTS `SelectPeopleWithParam`;
+DROP PROCEDURE IF EXISTS `CountPeople`;
+DROP PROCEDURE IF EXISTS `CountPeopleWithParam`;
+DROP PROCEDURE IF EXISTS `UpdatePeople`;
+DROP PROCEDURE IF EXISTS `UpdatePeopleWithParam`;
+
+
+CREATE PROCEDURE `SelectPeople` ()
+	SELECT * FROM `People`;
+
+CREATE PROCEDURE `SelectPeopleWithParam` (age INT)
+	SELECT * FROM `People` WHERE `People`.`Age` > age;
+
+CREATE PROCEDURE `CountPeople` () 
+	SELECT COUNT(*) FROM `People`;
+
+CREATE PROCEDURE `CountPeopleWithParam` (age INT)
+	SELECT COUNT(*) FROM `People` WHERE `People`.`Age` > age;
+
+CREATE PROCEDURE `UpdatePeople` () 
+	UPDATE `People` SET `FullName` = 'Updated'; 
+
+CREATE PROCEDURE `UpdatePeopleWithParam` (age INT)
+	UPDATE `People` SET `FullName` = 'Updated' WHERE `People`.`Age` > age;
