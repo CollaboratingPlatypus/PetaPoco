9357a4061a421bf22e460e20aeb5ed387c6ec5ca
Aaron Sherber <aaron@sherber.com>
2018-11-05 19:31:03 -0500

    Add Firebird integration tests

---
+++ PetaPoco.Tests.Integration/Databases/Firebird/FirebirdStoredProcTests.cs
@@ -0,0 +1,24 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using System;
+// <copyright company="PetaPoco - CollaboratingPlatypus">
+//      Apache License, Version 2.0 https://github.com/CollaboratingPlatypus/PetaPoco/blob/master/LICENSE.txt
+// </copyright>
+// <author>PetaPoco - CollaboratingPlatypus</author>
+// <date>2018/07/02</date>
+
+using System;
+using Xunit;
+
+namespace PetaPoco.Tests.Integration.Databases.Firebird
+{
+    [Collection("Firebird")]
+    public class FirebirdStoredProcTests : BaseStoredProcTests
+    {
+        public FirebirdStoredProcTests()
+           : base(new FirebirdDBTestProvider())
+        {
+        }
+
+        protected override Type DataParameterType => typeof(FbParameter);
+    }
+}
