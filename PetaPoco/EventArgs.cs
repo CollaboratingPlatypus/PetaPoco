9d69cfe8b29f1c2ed15d0606adddf93e09ea6063
Aaron Sherber <aaron@sherber.com>
2018-11-21 11:50:47 -0500

    Add events

---
+++ PetaPoco/EventArgs.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetaPoco
+{
+    public class DbTransactionEventArgs: EventArgs
+    {
+        public IDbTransaction Transaction { get; private set; }
+
+        public DbTransactionEventArgs(IDbTransaction transaction)
+        {
+            Transaction = transaction;
+        }
+    }
+
+    public class DbCommandEventArgs: EventArgs
+    {
+        public IDbCommand Command { get; private set; }
+
+        public DbCommandEventArgs(IDbCommand cmd)
+        {
+            Command = cmd;
+        }
+    }
+
+    public class DbConnectionEventArgs : EventArgs
+    {
+        public IDbConnection Connection { get; set; }
+
+        public DbConnectionEventArgs(IDbConnection connection)
+        {
+            Connection = connection;
+        }
+    }
+
+    public class ExceptionEventArgs: EventArgs
+    {
+        public bool Raise { get; set; } = true;
+        public Exception Exception { get; private set; }
+
+        public ExceptionEventArgs(Exception ex)
+        {
+            Exception = ex;
+        }
+    }
+}
