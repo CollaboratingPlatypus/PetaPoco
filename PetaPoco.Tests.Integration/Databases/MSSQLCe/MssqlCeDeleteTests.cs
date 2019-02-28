0bffa5a6acd03064eee09c267e0d62571f66dca2
Wade Baglin <plebmail@gmail.com>
2019-02-28 20:33:25 +1000

    Standardise xunit collection names

--- PetaPoco.Tests.Integration/Databases/MSSQLCe/MssqlCeDeleteTests.cs
+++ PetaPoco.Tests.Integration/Databases/MSSQLCe/MssqlCeDeleteTests.cs
@@ -8,7 +8,7 @@ using Xunit;
 
 namespace PetaPoco.Tests.Integration.Databases.MSSQLCe
 {
-    [Collection("MssqlCeTests")]
+    [Collection("MssqlCe")]
     public class MssqlCeDeleteTests : BaseDeleteTests
     {
         public MssqlCeDeleteTests()
