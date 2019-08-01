aa73080afa981a1c06ee981a20f99d89d8c6bfea
Argo Zhang <ArgoZhang@users.noreply.github.com>
2019-08-02 06:33:24 +0800

    fix(#540): Mapper register method throw ArgumentException (#541)
    * fix(#540): Mapper register method throw ArgumentException
    
    #Issue
    link https://github.com/CollaboratingPlatypus/PetaPoco/issues/540
    
    * fix: GetMapper check both type and assembly in the key

---
+++ PetaPoco.Tests.Unit/Core/MapperTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PetaPoco.Tests.Unit.Core
+{
+    public class MapperTest
+    {
+        private class StringMapperTest : ConventionMapper
+        {
+            public StringMapperTest()
+            {
+                Thread.Sleep(2000);
+            }
+        }
+
+        [Fact]
+        public void Register_Ok()
+        {
+            NoThrowExceptionMethod();
+        }
+
+        private void NoThrowExceptionMethod()
+        {
+            var t1 = Task.Run(() => RegisterMethod());
+            var t2 = Task.Run(() => RegisterMethod());
+            Task.WaitAll(t1, t2);
+        }
+
+        private void RegisterMethod()
+        {
+            if (Mappers.GetMapper(typeof(string), null) == null) Mappers.Register(typeof(string), new StringMapperTest());
+        }
+    }
+}
