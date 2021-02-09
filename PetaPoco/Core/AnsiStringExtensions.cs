98eaa937fd6e898b2bbd5314488bbd581319557a
Aaron Sherber <aaron@sherber.com>
2021-02-09 12:10:38 -0500

    Add some syntactic sugar to AnsiString

---
+++ PetaPoco/Core/AnsiStringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetaPoco
+{
+    public static class AnsiStringExtensions
+    {
+        /// <summary>
+        /// Converts an object to its <see cref="AnsiString"/> representation
+        /// </summary>
+        /// <param name="o">The object to be converted</param>
+        /// <returns></returns>
+        public static AnsiString ToAnsiString(this object o) => new AnsiString(o.ToString());
+    }
+}
