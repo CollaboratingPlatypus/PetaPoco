acb36ca496989ae9b0b86a49d270f592f4fde67d
Wade <plebmail@gmail.com>
2021-08-24 19:27:49 +1000

    Revert "add alternate non-IL Factory method to PocoData.cs. This is usable under iOS, which blocks IL generation and some reflection operations"
    This reverts commit d1c2fed4888eeb22d32886fa348c8c9d1d94ae3b.

--- PetaPoco/Database.cs
+++ PetaPoco/Database.cs
@@ -1371,8 +1371,9 @@ namespace PetaPoco
                     }
 
                     var readerAsync = reader as DbDataReader;
-                    var factory = pd.PocoFactory<T>(cmd.CommandText, _sharedConnection.ConnectionString, 0, reader.FieldCount, reader,
-                        _defaultMapper) as Func<IDataReader, T>;
+                    var factory =
+                        pd.GetFactory(cmd.CommandText, _sharedConnection.ConnectionString, 0, reader.FieldCount, reader,
+                            _defaultMapper) as Func<IDataReader, T>;
 
                     using (reader)
                     {
@@ -1440,8 +1441,8 @@ namespace PetaPoco
                 return AsyncReader<T>.Empty();
             }
 
-            var factory = pd.PocoFactory<T>(cmd.CommandText, _sharedConnection.ConnectionString, 0, reader.FieldCount, reader,
-                _defaultMapper) as Func<IDataReader, T>;
+            var factory =
+                pd.GetFactory(cmd.CommandText, _sharedConnection.ConnectionString, 0, reader.FieldCount, reader, _defaultMapper) as Func<IDataReader, T>;
 
             return new AsyncReader<T>(this, cmd, reader, factory);
         }
@@ -1468,7 +1469,7 @@ namespace PetaPoco
                         yield break;
                     }
 
-                    var factory = pd.PocoFactory<T>(cmd.CommandText, _sharedConnection.ConnectionString, 0, r.FieldCount, r,
+                    var factory = pd.GetFactory(cmd.CommandText, _sharedConnection.ConnectionString, 0, r.FieldCount, r,
                         _defaultMapper) as Func<IDataReader, T>;
                     using (r)
                     {
