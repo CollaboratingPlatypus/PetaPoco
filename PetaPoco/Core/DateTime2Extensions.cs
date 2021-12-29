20a42022aa629890c8d2044506f3179fb066f6ee
Marcel Roggendorf <Marcel.Roggendorf@dmi.de>
2021-12-29 07:30:14 +0100

    DateTime2Extensions String to DateTime2

--- PetaPoco/Core/DateTime2Extensions.cs
+++ PetaPoco/Core/DateTime2Extensions.cs
@@ -10,5 +10,11 @@ namespace PetaPoco
         /// <param name="o">The object to be converted</param>
         /// <returns></returns>
         public static DateTime2 ToDateTime2(this object o) => new DateTime2((DateTime)o);
+        /// <summary>
+        /// Parse a string to its <see cref="DateTime2"/> representation
+        /// </summary>
+        /// <param name="inputStr">The string to be converted</param>
+        /// <returns></returns>
+        public static DateTime2 ToDateTime2(this string inputStr) => new DateTime2(DateTime.Parse(inputStr));
     }
 }
