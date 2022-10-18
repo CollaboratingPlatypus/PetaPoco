b60f74a688a1a26a959ca776a62b192466c39c00
Wade <plebmail@gmail.com>
2022-10-18 15:43:33 +1000

    Only use one mysql server

--- PetaPoco.Tests.Integration/Databases/MySQLConnector/MySqlConnectorDBTestProvider.cs
+++ PetaPoco.Tests.Integration/Databases/MySQLConnector/MySqlConnectorDBTestProvider.cs
@@ -2,8 +2,24 @@ namespace PetaPoco.Tests.Integration.Databases.MySqlConnector
 {
     public class MySqlConnectorDBTestProvider : DBTestProvider
     {
-        protected override string ConnectionName => "mysqlconnector";
+        private string _connectionName = "mysqlconnector";
+        protected override string ConnectionName => _connectionName;
 
         protected override string ScriptResourceName => "PetaPoco.Tests.Integration.Scripts.MySqlBuildDatabase.sql";
+
+        public override IDatabase Execute()
+        {
+            EnsureDatabaseExists();
+            return base.Execute();
+        }
+
+        private void EnsureDatabaseExists()
+        {
+            _connectionName = "mysql_builder";
+            Database.Execute("CREATE DATABASE IF NOT EXISTS petapocoConnector");
+            Database.Execute("CREATE USER IF NOT EXISTS 'petapocoConnector'@'%' IDENTIFIED BY 'petapoco'");
+            Database.Execute("GRANT ALL PRIVILEGES ON petapocoConnector.* TO 'petapocoConnector'@'%'");
+            _connectionName = "mysqlconnector";
+        }
     }
 }
