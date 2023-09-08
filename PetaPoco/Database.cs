CollaboratingPlatypus/PetaPoco

commit 32ea9ca01dc3c5ae2a1a988f43e4aad80619f3fe

removed PropertyInfo param

PetaPoco/Database.cs
@@ -591,13 +591,13 @@ namespace PetaPoco
         /// <param name="cmd">A reference to the IDbCommand to which the parameter is to be added</param>
         /// <param name="value">The value to assign to the parameter</param>
         /// <param name="pi">Optional, a reference to the property info of the POCO property from which the value is coming.</param>
-        private void AddParam(IDbCommand cmd, object value, PropertyInfo pi, PocoColumn pc)
+        private void AddParam(IDbCommand cmd, object value, PocoColumn pc)
         {
             // Convert value to from poco type to db type
-            if (pi != null)
+            if (pc != null)
             {
-                var mapper = Mappers.GetMapper(pi.DeclaringType, _defaultMapper);
-                var fn = mapper.GetToDbConverter(pi);
+                var mapper = Mappers.GetMapper(pc.PropertyInfo.DeclaringType, _defaultMapper);
+                var fn = mapper.GetToDbConverter(pc.PropertyInfo);
                 if (fn != null)
                     value = fn(value);
             }
@@ -616,20 +616,20 @@ namespace PetaPoco
             {
                 var p = cmd.CreateParameter();
                 p.ParameterName = cmd.Parameters.Count.EnsureParamPrefix(_paramPrefix);
-                SetParameterProperties(p, value, pi, pc);
+                SetParameterProperties(p, value, pc);
 
                 cmd.Parameters.Add(p);
             }
         }
 
-        private void SetParameterProperties(IDbDataParameter p, object value, PropertyInfo pi, PocoColumn pc)
+        private void SetParameterProperties(IDbDataParameter p, object value, PocoColumn pc)
         {
             // Assign the parameter value
             if (value == null)
             {
                 p.Value = DBNull.Value;
 
-                if (pi?.PropertyType.Name == "Byte[]")
+                if (pc?.PropertyInfo.PropertyType.Name == "Byte[]")
                     p.DbType = DbType.Binary;
             }
             else
@@ -749,7 +749,7 @@ namespace PetaPoco
             cmd.CommandText = sql;
 
             foreach (var item in args)
-                AddParam(cmd, item, null, null);
+                AddParam(cmd, item, null);
 
             return cmd;
         }
@@ -1895,7 +1895,7 @@ namespace PetaPoco
 
                 names.Add(_provider.EscapeSqlIdentifier(i.Key));
                 values.Add(string.Format(i.Value.InsertTemplate ?? "{0}{1}", _paramPrefix, index++));
-                AddParam(cmd, i.Value.GetValue(poco), i.Value.PropertyInfo, i.Value);
+                AddParam(cmd, i.Value.GetValue(poco), i.Value);
             }
 
             var outputClause = string.Empty;
@@ -2152,7 +2152,7 @@ namespace PetaPoco
                     sb.AppendFormat(i.Value.UpdateTemplate ?? "{0} = {1}{2}", _provider.EscapeSqlIdentifier(i.Key), _paramPrefix, index++);
 
                     // Store the parameter in the command
-                    AddParam(cmd, i.Value.GetValue(poco), i.Value.PropertyInfo, i.Value);
+                    AddParam(cmd, i.Value.GetValue(poco), i.Value);
                 }
             }
             else
@@ -2167,7 +2167,7 @@ namespace PetaPoco
                     sb.AppendFormat(pc.UpdateTemplate ?? "{0} = {1}{2}", _provider.EscapeSqlIdentifier(colname), _paramPrefix, index++);
 
                     // Store the parameter in the command
-                    AddParam(cmd, pc.GetValue(poco), pc.PropertyInfo, pc);
+                    AddParam(cmd, pc.GetValue(poco), pc);
                 }
 
                 // Grab primary key value
@@ -2179,17 +2179,18 @@ namespace PetaPoco
             }
 
             // Find the property info for the primary key
-            PropertyInfo pkpi = null;
+            //PropertyInfo pkpi = null;
             PocoColumn col = null;
             if (primaryKeyName != null)
             {
                 //PocoColumn col;
-                pkpi = pd.Columns.TryGetValue(primaryKeyName, out col) ? col.PropertyInfo : new { Id = primaryKeyValue }.GetType().GetProperties()[0];
+                //pkpi = pd.Columns.TryGetValue(primaryKeyName, out col) ? col.PropertyInfo : new { Id = primaryKeyValue }.GetType().GetProperties()[0];
+                pd.Columns.TryGetValue(primaryKeyName, out col);
             }
 
             cmd.CommandText =
                 $"UPDATE {_provider.EscapeTableName(tableName)} SET {sb} WHERE {_provider.EscapeSqlIdentifier(primaryKeyName)} = {_paramPrefix}{index++}";
-            AddParam(cmd, primaryKeyValue, pkpi, col);
+            AddParam(cmd, primaryKeyValue, col);
         }
 
 #if ASYNC

PetaPoco/Utilities/ParametersHelper.cs
@@ -107,7 +107,7 @@ namespace PetaPoco.Internal
             return (input as System.Collections.IEnumerable) != null && (input as string) == null && (input as byte[]) == null;
         }
 
-        public static object[] ProcessStoredProcParams(IDbCommand cmd, object[] args, Action<IDbDataParameter, object, PropertyInfo, PocoColumn> setParameterProperties)
+        public static object[] ProcessStoredProcParams(IDbCommand cmd, object[] args, Action<IDbDataParameter, object, PocoColumn> setParameterProperties)
         {
             // For a stored proc, we assume that we're only getting POCOs or parameters
             var result = new List<IDbDataParameter>();
@@ -152,7 +152,7 @@ namespace PetaPoco.Internal
             {
                 var param = cmd.CreateParameter();
                 param.ParameterName = name;
-                setParameterProperties(param, value, null, null);
+                setParameterProperties(param, value, null);
                 result.Add(param);
             }
 

commit 711cdebd2f5787a8d4de8f6fd78d1f914de320f5

Fix issues with dynamic introduced in 32ea9ca

PetaPoco/Database.cs
@@ -599,7 +599,7 @@ namespace PetaPoco
         private void AddParam(IDbCommand cmd, object value, PocoColumn pc)
         {
             // Convert value to from poco type to db type
-            if (pc != null)
+            if (pc?.PropertyInfo != null)
             {
                 var mapper = Mappers.GetMapper(pc.PropertyInfo.DeclaringType, _defaultMapper);
                 var fn = mapper.GetToDbConverter(pc.PropertyInfo);
@@ -2255,13 +2255,14 @@ namespace PetaPoco
             }
 
             // Find the property info for the primary key
-            //PropertyInfo pkpi = null;
             PocoColumn col = null;
             if (primaryKeyName != null)
             {
-                //PocoColumn col;
-                //pkpi = pd.Columns.TryGetValue(primaryKeyName, out col) ? col.PropertyInfo : new { Id = primaryKeyValue }.GetType().GetProperties()[0];
-                pd.Columns.TryGetValue(primaryKeyName, out col);
+                if (!pd.Columns.TryGetValue(primaryKeyName, out col))
+                {
+                    var pkpi = new { Id = primaryKeyValue }.GetType().GetProperty("Id");
+                    col = new PocoColumn() { PropertyInfo = pkpi };
+                }
             }
 
             cmd.CommandText =