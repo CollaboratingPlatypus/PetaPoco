77acb79f6516a99e952f3a4d99e84fa70fac6c7c
Stelio Kontos <37424493+Ste1io@users.noreply.github.com>
2023-10-05 22:05:00 -0500

    generate MSAccess .accdb file programmatically

--- PetaPoco.Tests.Integration/Databases/MSAccessTests/MSAccessDbProviderFactory.cs
+++ PetaPoco.Tests.Integration/Databases/MSAccessTests/MSAccessDbProviderFactory.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Configuration;
+using System.Runtime.InteropServices;
 
 namespace PetaPoco.Tests.Integration.Databases.MSAccess
 {
@@ -8,6 +11,25 @@ namespace PetaPoco.Tests.Integration.Databases.MSAccess
         protected override string ConnectionName => "MSAccess";
 
         protected override string ScriptResourceName => "PetaPoco.Tests.Integration.Scripts.MSAccessBuildDatabase.sql";
+
+        public override IDatabase Execute()
+        {
+            if (!File.Exists(Path.Combine(Environment.CurrentDirectory, "PetaPoco.accdb")))
+            {
+                var catalog = new ADOX.Catalog();
+                
+                try
+                {
+                    catalog.Create(ConfigurationManager.ConnectionStrings["MSAccess"].ConnectionString);
+                }
+                finally
+                {
+                    Marshal.ReleaseComObject(catalog);
+                }
+            }
+
+            return base.Execute();
+        }
 
         public override void ExecuteBuildScript(IDatabase database, string script)
         {
